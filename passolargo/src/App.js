1e4a8d8eb2a207a796856479a3c33b1c636be5c6
Lougge <94178865+Lougge@users.noreply.github.com>
2022-11-23 18:54:09 -0300

    Update App.js

--- passolargo/src/App.js
+++ passolargo/src/App.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    <Button variant="primary">Primary</Button>{' '}
     </div>
   );
 }
