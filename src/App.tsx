d1f6936174e4c356d66bdcf5b56fac4f45cef9b0
Cabraiz <mateusccabr@gmail.com>
2023-01-17 05:34:11 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,7 @@ function App() {
               }}
             >
               <Col>
-                <Nav.Link href="#action1">HOME</Nav.Link>
+                <Nav.Link href="#action1">HOME2</Nav.Link>
               </Col>
               <Col>
                 <Nav.Link href="#action1">ABOUT</Nav.Link>
