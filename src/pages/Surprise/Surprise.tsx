e31a9e429ff768ff5d0322cc9b5a75b9b78d9656
Cabraiz <mateusccabr@gmail.com>
2023-02-06 20:20:48 -0300

    .

--- src/pages/Surprise/Surprise.tsx
+++ src/pages/Surprise/Surprise.tsx
@@ -1,22 +1,19 @@
 import React from "react";
-import { motion } from "framer-motion";
 import "./Surprise.css";
-import { Col, Row, Image } from "react-bootstrap";
+import { Col, Row, Image, Card } from "react-bootstrap";
 
 import icognita from "../../images/Surprise/Icognita.png";
 
 function Surprise() {
   return (
-    <Row className="justify-content-sm-start m-0 p-0 ">
-      <Col
-        className="p-0 text-nowrap"
-        style={{ marginTop: "5vh", background: "white" }}
-      >
-        <Image src={icognita} style={{ height: "85vh" }}></Image>
-        <Image src={icognita} style={{ height: "85vh" }}></Image>
-        <Image src={icognita} style={{ height: "85vh" }}></Image>
-        <Image src={icognita} style={{ height: "85vh" }}></Image>
-        <Image src={icognita} style={{ height: "85vh" }}></Image>
+    <Row className="m-0 p-0">
+      <Col className="p-0 text-nowrap" style={{ marginTop: "5vh" }}>
+        <Card style={{ height: "85vh", width: "10vw" }}></Card>
+        <Card style={{ height: "85vh", width: "10vw" }}></Card>
+        <Card style={{ height: "85vh", width: "10vw" }}></Card>
+        <Card style={{ height: "85vh", width: "10vw" }}></Card>
+        <Card style={{ height: "85vh", width: "10vw" }}></Card>
+        <Card style={{ height: "85vh", width: "10vw" }}></Card>
       </Col>
     </Row>
   );
