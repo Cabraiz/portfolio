e2dbf59ddaa84d1db5dcace6a97b4799def94fdf
Cabraiz <mateusccabr@gmail.com>
2023-02-06 04:13:05 -0300

    .

--- src/pages/Surprise/Surprise.tsx
+++ src/pages/Surprise/Surprise.tsx
@@ -8,7 +8,10 @@ import icognita from "../../images/Surprise/Icognita.png";
 function Surprise() {
   return (
     <Row className="justify-content-sm-start m-0 p-0 ">
-      <Col className="p-0" style={{ marginTop: "5vh" }}>
+      <Col
+        className="p-0 text-nowrap"
+        style={{ marginTop: "5vh", background: "white" }}
+      >
         <Image src={icognita} style={{ height: "85vh" }}></Image>
         <Image src={icognita} style={{ height: "85vh" }}></Image>
         <Image src={icognita} style={{ height: "85vh" }}></Image>
