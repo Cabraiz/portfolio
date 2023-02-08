7da4f4a5e0e3e3795e33b4b4a1f26a006323175a
Cabraiz <mateusccabr@gmail.com>
2023-02-07 23:29:49 -0300

    .

--- src/pages/Surprise/Surprise.tsx
+++ src/pages/Surprise/Surprise.tsx
@@ -4,7 +4,7 @@ import { Col, Row, Form } from "react-bootstrap";
 import icognita from "../../images/Surprise/Icognita.png";
 
 function Surprise() {
-  const [allColor, setColor] = useState([20, 40, 60, 80, 100]);
+  const [allColor, setColor] = useState(["20", "40", "60", "80", "100"]);
   return (
     <Row className="m-0 p-0 ">
       <Col className="p-0 text-nowrap" style={{ marginTop: "5vh" }}>
@@ -12,35 +12,35 @@ function Surprise() {
           //onMouseEnter={() => this.someHandler}
           value="?"
           style={{
-            backgroundColor: "rgb({allColor[0]}, {allColor[0]}, {allColor[0]})",
+            backgroundColor: `rgb(${allColor[0]}, ${allColor[0]} , ${allColor[0]})`,
           }}
         />
 
         <Form.Control
           value="?"
           style={{
-            backgroundColor: "rgb(allColor[1], allColor[1], allColor[1])",
+            backgroundColor: `rgb(${allColor[1]}, ${allColor[1]} , ${allColor[1]})`,
           }}
         />
 
         <Form.Control
           value="?"
           style={{
-            backgroundColor: "rgb(allColor[2], allColor[2], allColor[2])",
+            backgroundColor: `rgb(${allColor[2]}, ${allColor[2]} , ${allColor[2]})`,
           }}
         />
 
         <Form.Control
           value="?"
           style={{
-            backgroundColor: "rgb(allColor[3], allColor[3], allColor[3])",
+            backgroundColor: `rgb(${allColor[3]}, ${allColor[3]} , ${allColor[3]})`,
           }}
         />
 
         <Form.Control
           value="?"
           style={{
-            backgroundColor: "rgb(allColor[4], allColor[4], allColor[4])",
+            backgroundColor: `rgb(${allColor[4]}, ${allColor[4]} , ${allColor[4]})`,
           }}
         />
       </Col>
