ed2bb76902237e014d595b4bbd898cffee9388ee
Cabraiz <mateusccabr@gmail.com>
2023-02-09 04:15:28 -0300

    .

--- src/pages/Surprise/Surprise.tsx
+++ src/pages/Surprise/Surprise.tsx
@@ -1,48 +1,62 @@
 import React, { useState } from "react";
-import { Col, Row, Form } from "react-bootstrap";
+import { Col, Row, Form, Button } from "react-bootstrap";
 
 import icognita from "../../images/Surprise/Icognita.png";
 
 function Surprise() {
-  const [allColor, setColor] = useState(["20", "40", "60", "80", "100"]);
+  const [allColor, setColor] = useState(["20", "20", "20", "20", "20"]);
+  function mudarCorEnter(a: number) {
+    allColor[a] = "40";
+    console.log(allColor);
+  }
+  function mudarCorLeave(a: number) {
+    allColor[a] = "20";
+    console.log(allColor);
+  }
   return (
     <Row className="m-0 p-0 ">
       <Col className="p-0 text-nowrap" style={{ marginTop: "5vh" }}>
-        <Form.Control
-          //onMouseEnter={() => this.someHandler}
-          value="?"
+        <Button
+          onMouseOver={() => mudarCorEnter(0)}
+          onMouseOut={() => mudarCorLeave(0)}
           style={{
             backgroundColor: `rgb(${allColor[0]}, ${allColor[0]} , ${allColor[0]})`,
           }}
-        />
+        >
+          ?
+        </Button>
 
-        <Form.Control
-          value="?"
+        <Button
           style={{
             backgroundColor: `rgb(${allColor[1]}, ${allColor[1]} , ${allColor[1]})`,
           }}
-        />
+        >
+          ?
+        </Button>
 
-        <Form.Control
-          value="?"
+        <Button
           style={{
             backgroundColor: `rgb(${allColor[2]}, ${allColor[2]} , ${allColor[2]})`,
           }}
-        />
+        >
+          ?
+        </Button>
 
-        <Form.Control
-          value="?"
+        <Button
           style={{
             backgroundColor: `rgb(${allColor[3]}, ${allColor[3]} , ${allColor[3]})`,
           }}
-        />
+        >
+          ?
+        </Button>
 
-        <Form.Control
-          value="?"
+        <Button
           style={{
             backgroundColor: `rgb(${allColor[4]}, ${allColor[4]} , ${allColor[4]})`,
           }}
-        />
+        >
+          ?
+        </Button>
       </Col>
     </Row>
   );
