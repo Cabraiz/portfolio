9ef3be9a2dde849ff94d271ea637502a1c9347fb
Cabraiz <mateusccabr@gmail.com>
2023-02-10 03:56:25 -0300

    .

--- src/pages/Surprise/Surprise.tsx
+++ src/pages/Surprise/Surprise.tsx
@@ -1,30 +1,48 @@
 import React, { useRef, useState } from "react";
 import { Col, Row, Form, Button } from "react-bootstrap";
+import { isMobile } from "react-device-detect";
 import ScratchCard from "react-scratchcard-v2";
 
 import icognita from "../../images/Surprise/Icognita.png";
 
+const vh = 85;
+const vw = 40;
+const percRasp = 90;
 function Surprise() {
   const [allColor, setColor] = useState(["20", "20", "20", "20", "20"]);
+  const [finalizados, setFinalizado] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
 
   const ref = useRef<ScratchCard>(null);
 
   const onClickReset = () => {
     ref.current && ref.current.reset();
   };
-  const vh = 18;
+
   const convertVhToPx = (vh: number) => {
     const oneVhInPx = window.innerHeight / 100;
     return oneVhInPx * vh;
   };
-  const vw = 85;
-  const convertVwToPx = (vw: number) => {
+
+  const convertVwToPx = (vw: number, a: number) => {
     const oneVhInPx = window.innerWidth / 100;
     let temp = oneVhInPx * vw;
     if (temp < 140) {
       temp = 140;
     }
-    console.log(temp);
+    if (!isMobile) {
+      temp = temp / 2.8;
+    }
+    if (finalizados[a]) {
+      temp = 0;
+    }
     return temp;
   };
 
@@ -38,159 +56,140 @@ function Surprise() {
   }
   return (
     <Row className="m-0 p-0">
-      <Col className="p-0 text-nowrap" style={{ marginTop: "5vh" }}>
-        <div className="inline">
+      <Col
+        className="p-0 ms-0 me-0 mb-0 text-nowrap"
+        style={{ marginTop: "5vh" }}
+      >
+        <div className="inline" style={{ marginRight: "-1.2vw" }}>
           <ScratchCard
-            width={convertVwToPx(vh)}
-            height={convertVhToPx(vw)}
+            width={convertVwToPx(vw, 0)}
+            height={convertVhToPx(vh)}
             image={icognita}
-            finishPercent={80}
+            finishPercent={percRasp}
             onComplete={() => console.log("complete")}
           >
             <Button
-              onMouseOver={() => mudarCorEnter(0)}
-              onMouseOut={() => mudarCorLeave(0)}
+              style={{
+                height: "100%",
+                width: "95%",
+              }}
             >
               ?
             </Button>
           </ScratchCard>
         </div>
 
-        <div className="inline">
+        <div className="inline" style={{ marginRight: "-1.2vw" }}>
           <ScratchCard
-            width={convertVwToPx(vh)}
-            height={convertVhToPx(vw)}
+            width={convertVwToPx(vw, 1)}
+            height={convertVhToPx(vh)}
             image={icognita}
-            finishPercent={80}
+            finishPercent={percRasp}
             onComplete={() => console.log("complete")}
           >
-            <div
+            <Button
               style={{
-                backgroundColor: `rgb(${allColor[0]}, ${allColor[0]} , ${allColor[0]})`,
+                height: "100%",
+                width: "95%",
               }}
             >
-              <Button
-                onMouseOver={() => mudarCorEnter(0)}
-                onMouseOut={() => mudarCorLeave(0)}
-              >
-                ?
-              </Button>
-            </div>
+              ?
+            </Button>
           </ScratchCard>
         </div>
 
-        <div className="inline">
+        <div className="inline" style={{ marginRight: "-1.2vw" }}>
           <ScratchCard
-            width={convertVwToPx(vh)}
-            height={convertVhToPx(vw)}
+            width={convertVwToPx(vw, 2)}
+            height={convertVhToPx(vh)}
             image={icognita}
-            finishPercent={80}
+            finishPercent={percRasp}
             onComplete={() => console.log("complete")}
           >
-            <div
+            <Button
               style={{
-                backgroundColor: `rgb(${allColor[0]}, ${allColor[0]} , ${allColor[0]})`,
+                height: "100%",
+                width: "95%",
               }}
             >
-              <Button
-                onMouseOver={() => mudarCorEnter(0)}
-                onMouseOut={() => mudarCorLeave(0)}
-              >
-                ?
-              </Button>
-            </div>
+              ?
+            </Button>
           </ScratchCard>
         </div>
 
-        <div className="inline">
+        <div className="inline" style={{ marginRight: "-1.2vw" }}>
           <ScratchCard
-            width={convertVwToPx(vh)}
-            height={convertVhToPx(vw)}
+            width={convertVwToPx(vw, 3)}
+            height={convertVhToPx(vh)}
             image={icognita}
-            finishPercent={80}
+            finishPercent={percRasp}
             onComplete={() => console.log("complete")}
           >
-            <div
+            <Button
               style={{
-                backgroundColor: `rgb(${allColor[0]}, ${allColor[0]} , ${allColor[0]})`,
+                height: "100%",
+                width: "95%",
               }}
             >
-              <Button
-                onMouseOver={() => mudarCorEnter(0)}
-                onMouseOut={() => mudarCorLeave(0)}
-              >
-                ?
-              </Button>
-            </div>
+              ?
+            </Button>
           </ScratchCard>
         </div>
 
-        <div className="inline">
+        <div className="inline" style={{ marginRight: "-1.2vw" }}>
           <ScratchCard
-            width={convertVwToPx(vh)}
-            height={convertVhToPx(vw)}
+            width={convertVwToPx(vw, 4)}
+            height={convertVhToPx(vh)}
             image={icognita}
-            finishPercent={80}
+            finishPercent={percRasp}
             onComplete={() => console.log("complete")}
           >
-            <div
+            <Button
               style={{
-                backgroundColor: `rgb(${allColor[0]}, ${allColor[0]} , ${allColor[0]})`,
+                height: "100%",
+                width: "95%",
               }}
             >
-              <Button
-                onMouseOver={() => mudarCorEnter(0)}
-                onMouseOut={() => mudarCorLeave(0)}
-              >
-                ?
-              </Button>
-            </div>
+              ?
+            </Button>
           </ScratchCard>
         </div>
 
-        <div className="inline">
+        <div className="inline" style={{ marginRight: "-1.2vw" }}>
           <ScratchCard
-            width={convertVwToPx(vh)}
-            height={convertVhToPx(vw)}
+            width={convertVwToPx(vw, 5)}
+            height={convertVhToPx(vh)}
             image={icognita}
-            finishPercent={80}
+            finishPercent={percRasp}
             onComplete={() => console.log("complete")}
           >
-            <div
+            <Button
               style={{
-                backgroundColor: `rgb(${allColor[0]}, ${allColor[0]} , ${allColor[0]})`,
+                height: "100%",
+                width: "95%",
               }}
             >
-              <Button
-                onMouseOver={() => mudarCorEnter(0)}
-                onMouseOut={() => mudarCorLeave(0)}
-              >
-                ?
-              </Button>
-            </div>
+              ?
+            </Button>
           </ScratchCard>
         </div>
 
-        <div className="inline">
+        <div className="inline" style={{ marginRight: "-1.2vw" }}>
           <ScratchCard
-            width={convertVwToPx(vh)}
-            height={convertVhToPx(vw)}
+            width={convertVwToPx(vw, 6)}
+            height={convertVhToPx(vh)}
             image={icognita}
-            finishPercent={80}
+            finishPercent={percRasp}
             onComplete={() => console.log("complete")}
           >
-            <div
+            <Button
               style={{
-                backgroundColor: `rgb(${allColor[0]}, ${allColor[0]} , ${allColor[0]})`,
+                height: "100%",
+                width: "95%",
               }}
             >
-              <Button
-                onMouseOver={() => mudarCorEnter(0)}
-                onMouseOut={() => mudarCorLeave(0)}
-              >
-                ?
-              </Button>
-            </div>
+              ?
+            </Button>
           </ScratchCard>
         </div>
       </Col>
