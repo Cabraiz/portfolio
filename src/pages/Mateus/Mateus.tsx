1b3a2abc3c8f01b5286bb88020f7b140ee2d5b0a
Cabraiz <mateusccabr@gmail.com>
2023-03-10 03:26:03 -0300

    .

--- src/pages/Mateus/Mateus.tsx
+++ src/pages/Mateus/Mateus.tsx
@@ -2,17 +2,35 @@ import React from "react";
 import { Col, Row, Image } from "react-bootstrap";
 import perfil from "../../images/Mateus/perfil.webp";
 
+import dotsArray from "../../images/icones/svg_dotsArray.svg";
+
 function Mateus() {
   return (
     <Row className="p-0 m-0">
-      <Col className="col-6">
-        Full Stack2 <br></br>Developer
+      <Col
+        className="col-6 font-face-gm"
+        style={{
+          paddingTop: "18vh",
+          paddingLeft: "7vw",
+        }}
+      >
+        Full Stack <br></br>Developer.
       </Col>
       <Col className="col-6 m-0">
         <Image
+          src={dotsArray}
+          style={{
+            marginLeft: "-2.2vw",
+            marginTop: "6vh",
+            position: "absolute",
+            width: "20vh",
+            height: "20vh",
+          }}
+        />
+        <Image
           src={perfil}
           style={{
-            marginTop: "12vh",
+            marginTop: "14vh",
             marginLeft: "3vw",
             width: "calc(28vmin + 22vh)",
             height: "calc(28vmin + 22vh)",
